e7afbd6c5ad47594c3c7ba89eef6d30c3a686552
É US D <141030516+nicolas231107@users.noreply.github.com>
2023-10-16 09:07:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < 9) {
+    
+    listaDeTeclas[0].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
