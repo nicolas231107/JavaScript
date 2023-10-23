bf66868146b5c1540cbf31d140b3f68ac9258c5d
É US D <141030516+nicolas231107@users.noreply.github.com>
2023-10-23 09:02:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto
-while (contador < 9) {
-    
-    listaDeTeclas[0].onclick = tocaSomPom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    contador = contador + 1;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-    console.log(contador);
+     tecla.onkeydown = function (evento) {
+         
+        if (evento.code === "Space" || evento.code === "Enter") {
+            tecla.classList.add("ativa");
+         }
+
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.remove("ativa");
+    }
 
 }
